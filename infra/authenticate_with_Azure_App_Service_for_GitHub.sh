52022a42cd5230bea8ceea1f9b1fdf069809bd3c
Benoit Moussaud <benoit@moussaud.org>
2024-12-02 14:02:30 +0000

    working directory is .

--- infra/authenticate_with_Azure_App_Service_for_GitHub.sh
+++ infra/authenticate_with_Azure_App_Service_for_GitHub.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Variables
 resourceGroup="azrambi"
-resourceGroup="rambi-agents"
+#resourceGroup="rambi-agents"
 appName="azrambi-b76s6utvi44xo"
-appName="azure-rambi"
+#appName="azure-rambi"
 subscriptionId="9479b396-5d3e-467a-b89f-ba8400aeb7dd"
 myApp="azure-rambi"
 

723979f66bac2829d5dd406305b4ea5b2726a348
Benoit Moussaud <benoit@moussaud.org>
2024-12-18 14:02:07 +0000

    # This is a combination of 2 commits. # This is the 1st commit message:
    WIP
    
    # This is the commit message #2:
    
    WIP2

---
+++ src/movie_poster/cli_post.py
@@ -0,0 +1,50 @@
+import json
+from azurerambi.movie_service import GenAiMovieService
+import requests
+import os
+from dotenv import load_dotenv
+import logging
+import time
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+#https://azrambi-openai-b76s6utvi44xo.openai.azure.com                /openai/deployments/gpt-4o/chat/completions?api-version=2024-08-01-preview 
+#https://azure-rambi-apim-b76s6utvi44xo.azure-api.net/azure-openai-api/openai/deployments/gpt-4o/chat/completions?api-version=2024-08-01-preview 
+
+
+def generate_image(poster_data: dict):
+    """Placeholder function to describe the image"""
+    logger.info("generate_image of image at %s", poster_data['title'])
+    endpoint = "http://localhost:8000/movie_poster/generate"
+
+    logger.info("Calling endpoint %s", endpoint)
+
+    response = requests.post(endpoint, json=poster_data, timeout=100)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        logger.error("Failed to retrieve data: %s %s", response.status_code, response.text)
+        return None
+
+if __name__ == "__main__":
+    all =[]
+    for attempt in range(1):
+        logger.info("Attempt %d", attempt + 1)
+        start_time = time.time()
+        poster_data = {
+            'title': 'The Blues Brothers',
+            'description': 'The poster features two characters dressed in black suits, white shirts, black ties, and black hats, standing side by side against a police lineup backdrop with height measurements. Each character is holding a sign resembling a mugshot placard. One placard reads "THE BLUES" with details: 01A4392, BLUES, ELWOOD, 6\'1", 220 lbs. The other placard reads "BROTHERS" with details: 01A4391, BLUES, JAKE, 5\'8", 225 lbs. The character with the "BROTHERS" placard has the name "JAKE" tattooed across the fingers of one hand. The overall aesthetic suggests a playful yet mischievous tone, hinting at a comedic narrative.',
+        }
+        result = generate_image(poster_data)
+        all.append(time.time() - start_time)
+        logger.info("Time taken: %s seconds", all[-1])
+
+    mean_time = sum(all) / len(all)
+    logger.info("Mean time taken: %s seconds", mean_time)
+
+    if result:
+        logger.info(result)
+    else:
+        logger.error("Failed to retrieve data")
+

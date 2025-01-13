ae45ac335a8fdf74cf4f4f669bc017b7447e0872
Benoit Moussaud <benoit@moussaud.org>
2025-01-13 10:04:22 +0000

    pack

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,3 +2,4 @@ pip3 install -r .devcontainer/requirements-dev.txt
 #pip3 install -e src
 
 
+(curl -sSL "https://github.com/buildpacks/pack/releases/download/v0.36.0/pack-v0.36.0-linux.tgz" | sudo tar -C /usr/local/bin/ --no-same-owner -xzv pack)
